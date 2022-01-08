1415bfad59bf278d21094981afbaaf85982e6566
Ricardo Quintão <rgquintao@gmail.com>
2022-01-08 14:29:04 -0300

    Retira linha importacao nao usada

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -2,7 +2,6 @@ import React, { useCallback, useRef } from 'react';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { ValidationError } from 'yup';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
 import getValidationErrors from '../../utils/getValidationErrors';
 import logoImg from '../../assets/logo.png';
